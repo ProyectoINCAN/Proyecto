18cf62d48c926540fdb285d8434dcc1c0d9f6c21
fabianpy <naibaf.martinez@gmail.com>
2017-03-22 18:14:27 -0400

    se agregaron models

--- apps/pacientes/models.py
+++ apps/pacientes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoDoc(models.Model):
+    codigo = models.CharField(max_length=3, blank=False)
+    descripcion = models.CharField(max_length=50, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["codigo"]
+        verbose_name = "Tipo de Documento"
+        verbose_name_plural = "Tipos de Documento"
+
+class Sexo(models.Model):
+    codigo = models.CharField(max_length=3, blank=False)
+    descripcion = models.CharField(max_length=30, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["descripcion"]
+        verbose_name = "Sexo"
+        verbose_name_plural = "Sexos"
+
+class EstadoCivil(models.Model):
+    codigo = models.CharField(max_length=3, blank=False)
+    nombre = models.CharField(max_length=30, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["descripcion"]
+        verbose_name = "Sexo"
+        verbose_name_plural = "Sexos"
