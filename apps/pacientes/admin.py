59256768b711af0cfa1f59c0cb6bd60beeb6af64
fabianpy <naibaf.martinez@gmail.com>
2017-03-22 19:18:32 -0400

    se agregaron models, se agregaron admins

--- apps/pacientes/admin.py
+++ apps/pacientes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.pacientes.models import TipoDoc, EstadoCivil
+
+
+#registrar usando decorador, para evitar usar el método register
+@admin.register(TipoDoc)
+class TipoDocAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'descripcion', 'habilitado']
+
+
+#registrar usando el método register
+class EstadoCivilAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'nombre', 'habilitado']
+admin.site.register(EstadoCivil, EstadoCivilAdmin)
+
--- apps/pacientes/models.py
+++ apps/pacientes/models.py
@@ -7,13 +7,18 @@ class TipoDoc(models.Model):
     descripcion = models.CharField(max_length=50, blank=False)
     habilitado = models.BooleanField(default=True)
 
+    #método para que retorne el objeto en formato de string
+    def __str__(self):
+        return self.descripcion
+
     class Meta:
         ordering = ["codigo"]
         verbose_name = "Tipo de Documento"
         verbose_name_plural = "Tipos de Documento"
 
+
 class Sexo(models.Model):
-    codigo = models.CharField(max_length=3, blank=False)
+    codigo = models.CharField(max_length=1, blank=False)
     descripcion = models.CharField(max_length=30, blank=False)
     habilitado = models.BooleanField(default=True)
 
@@ -23,11 +28,27 @@ class Sexo(models.Model):
         verbose_name_plural = "Sexos"
 
 class EstadoCivil(models.Model):
-    codigo = models.CharField(max_length=3, blank=False)
+    codigo = models.CharField(max_length=2, blank=False)
     nombre = models.CharField(max_length=30, blank=False)
     habilitado = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.nombre
+
     class Meta:
-        ordering = ["descripcion"]
-        verbose_name = "Sexo"
-        verbose_name_plural = "Sexos"
+        ordering = ["nombre"]
+        verbose_name = "Estado Civil"
+        verbose_name_plural = "Estados Civiles"
+
+class NivelEducativo(models.Model):
+    nombre = models.CharField(max_length=50, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Nivel Educativo"
+        verbose_name_plural = "Niveles Educativos"
+
