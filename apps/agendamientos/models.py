c789c1b16cb3fe5c8be7e3d13043df671eff99eb
fabianpy <naibaf.martinez@gmail.com>
2017-04-06 18:35:06 -0400

    se cambió antecedentes socioeconómicos a model Paciente, se agregaron models a agendamiento

--- apps/agendamientos/models.py
+++ apps/agendamientos/models.py
@@ -1,168 +1,52 @@
 from django.db import models
 
 # Create your models here.
+from apps.consultorios.models import Medico, Turno, Consultorio
 from apps.pacientes.models import Paciente
 
 
-class Pared(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
+class EstadoAgenda(models.Model):
+    codigo = models.CharField(max_length=1, blank=False, primary_key=True)
+    nombre = models.CharField(max_length=50, blank=False)
 
     def __str__(self):
         return self.nombre
 
     class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Pared"
-        verbose_name_plural = "Paredes"
+        ordering = ['nombre']
+        verbose_name = 'Estado agenda'
+        verbose_name_plural = 'Estados de agenda'
 
 
-class Techo(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Techo"
-        verbose_name_plural = "Techos"
-
-
-class Piso(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
+class Agenda(models.Model):
+    medico = models.ForeignKey(Medico, models.DO_NOTHING, blank=False, null=False)
+    fecha = models.DateField(auto_now=False, blank=False, null=False) #TODO hacer funcion para que estire la siguiente fecha disponible del médico
+    turno = models.ForeignKey(Turno, models.DO_NOTHING, blank=False, null=False)
+    consultorio = models.ForeignKey(Consultorio, models.DO_NOTHING, blank=True, null=True)
+    cantidad = models.IntegerField(default=20) #puede estar parametrizado
+    estado = models.ForeignKey(EstadoAgenda, models.DO_NOTHING, blank=False, null=False) #TODO agregar default Pendiente que traiga de EstadoAgenda
 
     def __str__(self):
-        return self.nombre
+        return self.fecha + ", Turno: " + self.turno
 
     class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Piso"
-        verbose_name_plural = "Pisos"
+        ordering = ['fecha', 'turno']
+        verbose_name = 'Agenda'
+        verbose_name_plural = 'Agendas'
 
 
-class Dependencia(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Dependencia"
-        verbose_name_plural = "Dependencias"
-
-
-class Agua(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Agua"
-        verbose_name_plural = "Aguas"
-
-
-class EliminacionBasura(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Eliminación Basura"
-        verbose_name_plural = "Eliminación de Basuras"
-
-
-class Desagua(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Desagua"
-        verbose_name_plural = "Desagua"
-
-
-class ServicioBasico(models.Model):
-    nombre = models.CharField(max_length=100, blank=False)
-    habilitado = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        ordering = ["nombre"]
-        verbose_name = "Servicio Básico"
-        verbose_name_plural = "Servicio Básico"
-
-
-class Vivienda(models.Model):
+class AgendaDetalle(models.Model):
+    agenda = models.ForeignKey(Agenda, models.DO_NOTHING, blank=False, null=False)
     paciente = models.ForeignKey(Paciente, models.DO_NOTHING, blank=False, null=False)
-    pared = models.ForeignKey(Pared, models.DO_NOTHING, blank=False, null=False)
-    techo = models.ForeignKey(Techo, models.DO_NOTHING, blank=False, null=False)
-    piso = models.ForeignKey(Piso, models.DO_NOTHING, blank=False, null=False)
-    dependencia = models.ForeignKey(Dependencia, models.DO_NOTHING, blank=False, null=False)
-    hacinamiento = models.BooleanField(default=True)
-    nro_personas_hogar = models.IntegerField(blank=False, verbose_name="Nro. de Personas en el Hogar")
-    comparte_cama = models.BooleanField(default=True)
+    orden = models.IntegerField() #TODO incremento automático según el orden de llegada
+    confirmado = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.id
+        return self.fecha + ", Turno: " + self.turno
 
     class Meta:
-        ordering = ["pared"]
-        verbose_name = "Vivienda"
-        verbose_name_plural = "Viviendas"
-
-
-class ServicioSanitario(models.Model):
-    paciente = models.ForeignKey(Paciente, models.DO_NOTHING, blank=False, null=False)
-    agua = models.ForeignKey(Agua, models.DO_NOTHING, blank=False, null=False)
-    eliminacion_basura = models.ForeignKey(EliminacionBasura, models.DO_NOTHING, blank=False, null=False)
-    desagua = models.ForeignKey(Desagua, models.DO_NOTHING, blank=False, null=False)
-
-    def __str__(self):
-        return self.paciente
-
-    class Meta:
-        ordering = ["paciente"]
-        verbose_name = "Servicio Sanitario"
-        verbose_name_plural = "Servicios Sanitarios"
-
-
-class ServicioBasicos(models.Model):
-    paciente = models.ForeignKey(Paciente, models.DO_NOTHING, blank=False, null=False)
-    luz_electrica = models.BooleanField(default=True)
-    telefono_linea_baja = models.BooleanField(default=True)
-    telefono_linea_ceular = models.BooleanField(default=True)
-    heladera = models.BooleanField(default=True)
-    televisor = models.BooleanField(default=True)
-    otros = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.paciente
-
-    class Meta:
-        ordering = ["paciente"]
-        verbose_name = "Servcio Básico"
-        verbose_name_plural = "Servicios Básicos"
-
-
-
-
-
-
+        ordering = ['agenda', 'orden']
+        verbose_name = 'Detalle de Agenda'
+        verbose_name_plural = 'Detalles de Agenda'
 
 
--- apps/pacientes/models.py
+++ apps/pacientes/models.py
@@ -425,3 +425,160 @@ class PacienteSeguroMedico(models.Model):
         ordering = ["seguro_medico"]
         verbose_name = "Paciente Seguro Médico"
         verbose_name_plural = "Paciente Seguro Médico"
+
+
+class Pared(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Pared"
+        verbose_name_plural = "Paredes"
+
+
+class Techo(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Techo"
+        verbose_name_plural = "Techos"
+
+
+class Piso(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Piso"
+        verbose_name_plural = "Pisos"
+
+
+class Dependencia(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Dependencia"
+        verbose_name_plural = "Dependencias"
+
+
+class Agua(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Agua"
+        verbose_name_plural = "Aguas"
+
+
+class EliminacionBasura(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Eliminación Basura"
+        verbose_name_plural = "Eliminación de Basuras"
+
+
+class Desague(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Desagua"
+        verbose_name_plural = "Desagua"
+
+
+class ServicioBasico(models.Model):
+    nombre = models.CharField(max_length=100, blank=False)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
+        verbose_name = "Servicio Básico"
+        verbose_name_plural = "Servicio Básico"
+
+
+class Vivienda(models.Model):
+    paciente = models.ForeignKey(Paciente, models.DO_NOTHING, blank=False, null=False)
+    pared = models.ForeignKey(Pared, models.DO_NOTHING, blank=False, null=False)
+    techo = models.ForeignKey(Techo, models.DO_NOTHING, blank=False, null=False)
+    piso = models.ForeignKey(Piso, models.DO_NOTHING, blank=False, null=False)
+    dependencia = models.ForeignKey(Dependencia, models.DO_NOTHING, blank=False, null=False)
+    hacinamiento = models.BooleanField(default=True)
+    nro_personas_hogar = models.IntegerField(blank=False, verbose_name="Nro. de Personas en el Hogar")
+    comparte_cama = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        ordering = ["pared"]
+        verbose_name = "Vivienda"
+        verbose_name_plural = "Viviendas"
+
+
+class ServicioSanitario(models.Model):
+    paciente = models.ForeignKey(Paciente, models.DO_NOTHING, blank=False, null=False)
+    agua = models.ForeignKey(Agua, models.DO_NOTHING, blank=False, null=False)
+    eliminacion_basura = models.ForeignKey(EliminacionBasura, models.DO_NOTHING, blank=False, null=False)
+    desagua = models.ForeignKey(Desague, models.DO_NOTHING, blank=False, null=False)
+
+    def __str__(self):
+        return self.paciente
+
+    class Meta:
+        ordering = ["paciente"]
+        verbose_name = "Servicio Sanitario"
+        verbose_name_plural = "Servicios Sanitarios"
+
+
+class ServicioBasicos(models.Model):
+    paciente = models.ForeignKey(Paciente, models.DO_NOTHING, blank=False, null=False)
+    luz_electrica = models.BooleanField(default=True)
+    telefono_linea_baja = models.BooleanField(default=True)
+    telefono_linea_ceular = models.BooleanField(default=True)
+    heladera = models.BooleanField(default=True)
+    televisor = models.BooleanField(default=True)
+    otros = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.paciente
+
+    class Meta:
+        ordering = ["paciente"]
+        verbose_name = "Servcio Básico"
+        verbose_name_plural = "Servicios Básicos"
+
