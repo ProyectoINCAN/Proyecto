f8e604d0e856b28cb4f89d63b0a7818d94ed6678
Jose Maria <jose251291@gmail.com>
2017-08-13 13:23:30 -0400

    Se agrego un toastMessage para el mensaje emergente(tipo alert) y la funcion para ver el permiso del usuario

--- apps/principal/common_functions.py
+++ apps/principal/common_functions.py
@@ -1,4 +1,5 @@
 
+from django.contrib.auth.models import User
 #funcion para establecer los parametros recibidos de los filtros
 def filtros_establecidos(request, tipo):
     if tipo == "index_paciente":
@@ -14,3 +15,40 @@ def filtros_establecidos(request, tipo):
         except Exception as e:
             print("Parametros no establecidos")
             return 0
+
+
+def ver_permisos(usuario_id, permiso_buscado):
+    """
+    Funcion para comprobar que un usuario tenga permisos habilitados sobre la vista
+    recibia como parametro
+    :param usuario: codigo de usuario
+    :param permiso_buscado: nombre del permiso
+    :return: True o False
+    """
+
+    print("Id_user = "+ usuario_id)
+
+    print("Permiso = "+ permiso_buscado)
+    #buscamos al usuario por el usuario_id
+    user= User.objects.get(id = usuario_id)
+
+    #obtenemos todos los permisos
+    list_permisos = user.get_all_permissions()
+    tiene_permiso = False
+
+    for permiso in list_permisos:
+        #el permiso de admeinistrador tiene acceso a todos
+        if user.groups.get().name == "Administrador":
+            tiene_permiso = True
+            break
+        else:
+            if permiso == permiso_buscado:
+                tiene_permiso = True
+                break
+    if tiene_permiso:
+        print("El usuario posee permiso")
+        permission = True
+    else:
+        print("El usuario no posee permiso")
+        permission = False
+    return permission
