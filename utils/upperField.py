882754264e6a32a9135a79b19468495589f51d1d
haide <haideg.93@gmail.com>
2017-09-16 14:16:09 -0400

    Se agrego el util upperField

---
+++ utils/upperField.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.six import with_metaclass
+
+class UpperCharField(with_metaclass(models.SubfieldBase, models.CharField)):
+    def __init__(self, *args, **kwargs):
+        self.is_uppercase = kwargs.pop('uppercase', False)
+        super(UpperCharField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        value = super(UpperCharField, self).get_prep_value(value)
+        if self.is_uppercase:
+            return value.upper()
+
+        return value
+
+class UpperTextField(with_metaclass(models.SubfieldBase, models.TextField)):
+    def __init__(self, *args, **kwargs):
+        self.is_uppercase = kwargs.pop('uppercase', False)
+        super(UpperTextField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        value = super(UpperTextField, self).get_prep_value(value)
+        if self.is_uppercase:
+            return value.upper()
+
+        return value
